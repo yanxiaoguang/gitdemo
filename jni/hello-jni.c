5d94facc42f7e782159cd2698b3b4c1599fd3ece
xiaoguang.yan <yoyoflash@qq.com>
2013-08-09 11:51:13 +0800

    modify hello-jni.c and second commit.

--- jni/hello-jni.c
+++ jni/hello-jni.c
@@ -3,5 +3,5 @@
 
 jstring Java_com_yoyoflash_hello_1jni_MainActivity_stringFromJNI(JNIEnv* env,
 		jobject thiz) {
-	return (*env)->NewStringUTF(env, "Hello from JNI haha 中文!");
+	return (*env)->NewStringUTF(env, "Hello from JNI haha 中文 modify !");
 }
